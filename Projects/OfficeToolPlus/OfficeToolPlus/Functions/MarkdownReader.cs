0d47754c96ba6ab7e2aff8510f69b82cd5beccf6
Yerong <YerongAI@users.noreply.github.com>
2019-07-23 19:58:37 +0800

    Update MarkdownReader.cs

--- Projects/OfficeToolPlus/OfficeToolPlus/Functions/MarkdownReader.cs
+++ Projects/OfficeToolPlus/OfficeToolPlus/Functions/MarkdownReader.cs
@@ -15,22 +15,25 @@ namespace OfficeTool.Functions
     // Only supported title, image, line, text color and text style.
     // Only used in Office Tool Plus.
     /*
-     Supported format
-     Title: 1, 2, 3, 4, 5, 6 (#, ##, ###, ####, #####, ######)
-     Red color text: `some text`
-     Oblique: *some text*
-     Bold: *some text*
-     Italic and bold: ***some text***
-     Highlight text: ==some text==
-     Line: Three * or - or more.
-     Hyper link: [Text](Link)
-     Image: [Image Tooltip](Link)
+     Supported format (支持的格式)
+     Title (标题): 1, 2, 3, 4, 5, 6 (#, ##, ###, ####, #####, ######)
+     Red color text (红色文本): `some text`
+     Oblique (斜体): *some text*
+     Bold (粗体): *some text*
+     Italic and bold (斜体加粗): ***some text***
+     Highlight text (高亮文本): ==some text==
+     Line (横线): Three * or - or more.
+     Hyper link (超链接): [Text](Link)
+     Image (图片): [Image Tooltip](Link)
      Code block (No highlight and grammar check, don't support type of codes):
      ```
      some text
      ```
      */
 
+    /// <summary>
+    /// 读取 Markdown 格式的文本，并以 Paragraph 类返回，用于 RichTextBox 中
+    /// </summary>
     class MarkdownReader
     {
         private readonly Paragraph paragraph = new Paragraph();
@@ -44,74 +47,50 @@ namespace OfficeTool.Functions
             {
                 if (line.StartsWith("# "))
                 {
-                    if (queue.Count > 0)
-                    {
-                        AddText(GetText(queue));
-                    }
+                    AddText(GetText(queue));
                     // 一级标题
                     AddTitle(line.Substring(2), 28, FontWeights.Normal);
                 }
                 else if (line.StartsWith("## "))
                 {
-                    if (queue.Count > 0)
-                    {
-                        AddText(GetText(queue));
-                    }
+                    AddText(GetText(queue));
                     // 二级标题
                     AddTitle(line.Substring(3), 24, FontWeights.Normal);
                 }
                 else if (line.StartsWith("### "))
                 {
-                    if (queue.Count > 0)
-                    {
-                        AddText(GetText(queue));
-                    }
+                    AddText(GetText(queue));
                     // 三级标题
                     AddTitle(line.Substring(4), 18, FontWeights.Normal);
                 }
                 else if (line.StartsWith("#### "))
                 {
-                    if (queue.Count > 0)
-                    {
-                        AddText(GetText(queue));
-                    }
+                    AddText(GetText(queue));
                     // 四级标题
                     AddTitle(line.Substring(5), 15, FontWeights.Bold);
                 }
                 else if (line.StartsWith("##### "))
                 {
-                    if (queue.Count > 0)
-                    {
-                        AddText(GetText(queue));
-                    }
+                    AddText(GetText(queue));
                     // 五级标题
                     AddTitle(line.Substring(6), 13, FontWeights.Bold);
                 }
                 else if (line.StartsWith("###### "))
                 {
-                    if (queue.Count > 0)
-                    {
-                        AddText(GetText(queue));
-                    }
+                    AddText(GetText(queue));
                     // 六级标题
                     AddTitle(line.Substring(7), 11, FontWeights.Bold);
                 }
                 else if ((line.Contains("---") && line.Replace("-", "").Length == 0) || (line.Contains("***") && line.Replace("*", "").Length == 0))
                 {
-                    if (queue.Count > 0)
-                    {
-                        AddText(GetText(queue));
-                    }
+                    AddText(GetText(queue));
                     // 横线
                     AddLine();
                     AddText("\n");
                 }
                 else if (line.Contains("```") && line.Replace("`", "").Length == 0)
                 {
-                    if (queue.Count > 0)
-                    {
-                        AddText(GetText(queue));
-                    }
+                    AddText(GetText(queue));
                     // 代码块（伪，无语法高亮，且只能单独复制）
                     string temp = string.Empty;
                     while ((line = reader.ReadLine()) != null)
@@ -261,10 +240,7 @@ namespace OfficeTool.Functions
                     }
                 }
             }
-            if (queue.Count > 0)
-            {
-                AddText(GetText(queue));
-            }
+            AddText(GetText(queue));
         }
 
         /// <summary>
@@ -296,11 +272,14 @@ namespace OfficeTool.Functions
         /// <param name="text">文本内容</param>
         private void AddText(string text)
         {
-            Run run = new Run
+            if (text.Length > 0)
             {
-                Text = text
-            };
-            paragraph.Inlines.Add(run);
+                Run run = new Run
+                {
+                    Text = text
+                };
+                paragraph.Inlines.Add(run);
+            }
         }
 
         /// <summary>
@@ -423,6 +402,11 @@ namespace OfficeTool.Functions
             paragraph.Inlines.Add(container);
         }
 
+        /// <summary>
+        /// 返回队列中所有的文字，队列中的文字会被清空
+        /// </summary>
+        /// <param name="queue">源队列</param>
+        /// <returns></returns>
         private string GetText(Queue queue)
         {
             string temp = string.Empty;
@@ -440,6 +424,9 @@ namespace OfficeTool.Functions
             return paragraph;
         }
 
+        /// <summary>
+        /// 文本的类型
+        /// </summary>
         private enum TextType
         {
             Oblique = 0,

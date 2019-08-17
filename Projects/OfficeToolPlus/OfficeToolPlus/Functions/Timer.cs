c7e8abc8441ec375b4f57476099d49eb87b8f8a8
Yerong <39847248+YerongL@users.noreply.github.com>
2019-08-18 01:19:08 +0800

    Create Timer.cs

---
+++ Projects/OfficeToolPlus/OfficeToolPlus/Functions/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OTP.Functions
+{
+    class Timer
+    {
+        private static DateTime dateTime = new DateTime();
+
+        public Timer(bool reset)
+        {
+            if (reset)
+            {
+                dateTime = DateTime.Now;
+            }
+        }
+
+        public string GetTimePass()
+        {
+            return DateTime.Now.Subtract(dateTime).ToString(@"mm\:ss");
+        }
+    }
+}

52361b5171d2597278bb48826a8fce34ebeb89d9
Yerong <YerongAI@users.noreply.github.com>
2019-11-03 18:45:11 +0800

    Update function

---
+++ Projects/OfficeToolPlus/OfficeToolPlus/Functions/ProgressBarExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace OfficeTool.Functions
+{
+    public static class ProgressBarExtensions
+    {
+        /// <summary>
+        /// 使用线性动画设置 Progress Bar 的值
+        /// </summary>
+        /// <param name="progressBar">Progress Bar</param>
+        /// <param name="percentage">值</param>
+        public static void SetPercent(this ProgressBar progressBar, double percentage)
+        {
+            DoubleAnimation animation = new DoubleAnimation(percentage, TimeSpan.FromMilliseconds(300))
+            {
+                DecelerationRatio = 0.7
+            };
+            progressBar.BeginAnimation(System.Windows.Controls.Primitives.RangeBase.ValueProperty, animation);
+        }
+    }
+}
--- Projects/OfficeToolPlus/OfficeToolPlus/Functions/Timer.cs
+++ Projects/OfficeToolPlus/OfficeToolPlus/Functions/Timer.cs
@@ -18,5 +18,14 @@ namespace OTP.Functions
         {
             return DateTime.Now.Subtract(dateTime).ToString(@"mm\:ss");
         }
+
+        public string GetElcapedTime(double totalSize, double downloadedSize)
+        {
+            long speed = (long)(downloadedSize / DateTime.Now.Subtract(dateTime).TotalSeconds);
+            if (speed == 0)
+                speed = 1;
+            DateTime time = new DateTime(((long)(totalSize - downloadedSize) / speed) * 10000000);
+            return time.ToString(@"mm\:ss");
+        }
     }
 }

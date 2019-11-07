b3fcb5759de8778b73e2dca59ef82192e6a7f351
Yerong <YerongAI@users.noreply.github.com>
2019-11-07 08:22:48 +0800

    Update ProgressBarExtensions.cs

--- Projects/OfficeToolPlus/OfficeToolPlus/Functions/ProgressBarExtensions.cs
+++ Projects/OfficeToolPlus/OfficeToolPlus/Functions/ProgressBarExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Animation;
 
@@ -17,6 +18,22 @@ namespace OfficeTool.Functions
             {
                 AccelerationRatio = 0,
                 DecelerationRatio = 0.6
+            };
+            progressBar.BeginAnimation(System.Windows.Controls.Primitives.RangeBase.ValueProperty, animation);
+        }
+
+        /// <summary>
+        /// 使用线性动画设置 Progress Bar 的值
+        /// </summary>
+        /// <param name="progressBar">Progress Bar</param>
+        /// <param name="percentage">值</param>
+        /// <param name="percentage">持续时间</param>
+        public static void SetPercent(this ProgressBar progressBar, double percentage, Duration duration)
+        {
+            DoubleAnimation animation = new DoubleAnimation(percentage, duration);
+            animation.Completed += (obj, args) =>
+            {
+                progressBar.Value = percentage;
             };
             progressBar.BeginAnimation(System.Windows.Controls.Primitives.RangeBase.ValueProperty, animation);
         }
